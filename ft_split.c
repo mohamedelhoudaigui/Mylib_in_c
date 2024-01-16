a0ccf820f130527768bede46306c5d1839230b40
Mohamed El Houdaigui <mel-houd@e2r10p14.1337.ma>
2024-01-16 01:28:09 +0100

    fixed split norm

--- ft_split.c
+++ ft_split.c
@@ -6,88 +6,88 @@
 /*   By: mel-houd <mel-houd@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/04 21:54:26 by mel-houd          #+#    #+#             */
-/*   Updated: 2023/11/10 19:53:36 by mel-houd         ###   ########.fr       */
+/*   Updated: 2024/01/16 01:27:50 by mel-houd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int      count_words(char const *s, char *delimiters)
+static int	count_words(char const *s, char *delimiters)
 {
-        int     i;
-        int     res;
+	int	i;
+	int	res;
 
-        i = 0;
-        res = 0;
-        while (s && s[i])
-        {
-                if (!ft_strchr(delimiters, s[i]))
-                {
-                        res++;
-                        while (*s && !ft_strchr(delimiters, *s))
-                                s++;
-                }
-                else
-                        s++;
-        }
-        return (res);
+	i = 0;
+	res = 0;
+	while (s && s[i])
+	{
+		if (!ft_strchr(delimiters, s[i]))
+		{
+			res++;
+			while (*s && !ft_strchr(delimiters, *s))
+				s++;
+		}
+		else
+			s++;
+	}
+	return (res);
 }
 
-static int      word_size(char const *s, char *delimiters, int i)
+static int	word_size(char const *s, char *delimiters, int i)
 {
-        int     res;
+	int	res;
 
-        res = 0;
-        while (s[i] && !ft_strchr(delimiters, s[i]))
-        {
-                res++;
-                i++;
-        }
-        return (res);
+	res = 0;
+	while (s[i] && !ft_strchr(delimiters, s[i]))
+	{
+		res++;
+		i++;
+	}
+	return (res);
 }
 
-static void     memory_free(char **strings, int max_index)
+static void	memory_free(char **strings, int max_index)
 {
-        while (max_index >= 0)
-                free(strings[max_index--]);
-        free(strings);
+	while (max_index >= 0)
+		free(strings[max_index--]);
+	free(strings);
 }
 
-static int      fill_string(char const *s,
-        char *delimiters, char **res, int number_words)
+static int	fill_string(char const *s,
+		char *delimiters, char **res, int number_words)
 {
-        int     i;
-        int     j;
-        int     size;
+	int	i;
+	int	j;
+	int	size;
 
-        i = 0;
-        j = 0;
-        while (j < number_words)
-        {
-                while (ft_strchr(delimiters, s[i]))
-                        i++;
-                size = word_size(s, delimiters, i);
-                res[j] = ft_substr(s, i, size);
-                if (res[j] == NULL)
-                {
-                        memory_free(res, j);
-                        return (1);
-                }
-                j++;
-                i += size;
-        }
-        res[j] = NULL;
-        return (0);
+	i = 0;
+	j = 0;
+	while (j < number_words)
+	{
+		while (ft_strchr(delimiters, s[i]))
+			i++;
+		size = word_size(s, delimiters, i);
+		res[j] = ft_substr(s, i, size);
+		if (res[j] == NULL)
+		{
+			memory_free(res, j);
+			return (1);
+		}
+		j++;
+		i += size;
+	}
+	res[j] = NULL;
+	return (0);
 }
 
-char    **ft_split(char const *s, char *delimiters)
+char	**ft_split(char const *s, char *delimiters)
 {
-        int             number_words;
-        char    **res;
+	int		number_words;
+	char	**res;
 
-        number_words = count_words(s, delimiters);
-        res = (char **)malloc(sizeof(char *) * (number_words + 1));
-        if (!res || fill_string(s, delimiters, res, number_words))
-                return (NULL);
-        return (res);
+	number_words = count_words(s, delimiters);
+	res = (char **)malloc(sizeof(char *) * (number_words + 1));
+	if (!res || fill_string(s, delimiters, res, number_words))
+		return (NULL);
+	return (res);
 }
